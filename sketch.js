0b427b8a7207839820ba814676b1638175b120d1
Anikait857 <71574731+Anikait857@users.noreply.github.com>
2021-03-31 17:22:33 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var happyDog
+var database
+var  foodS
+var foodStock
+function preload()
+{
+happyDog.loadImage("dogImg.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+  background()
+
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+happyDog = createSprite(200,200,10,10)
+}
+
+
+
